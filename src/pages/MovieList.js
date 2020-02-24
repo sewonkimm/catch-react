651aa3d464214a70cce2fb2aa5c377e8162956da
SeWonKim <swon962@gmail.com>
2020-02-24 21:00:00 +0900

    get data from server

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -6,6 +6,28 @@ export default class extends React.Component {
     movieList: []
   };
 
+  // MovieList.js 파일
+  componentDidMount = async () => {
+
+  // 데이터를 불러올 API 주소
+  let url = "http://54.180.149.147:8080/api/movie";
+    
+  //데이터를 서버에서 불러와서 movieList라는 변수에 저장한다 
+  let movieList = await superagent
+    .get(url)
+    .then(response => response.body)
+    .catch(error => error);
+    
+  //불러온 데이터를 브라우저에 임시로 저장한다
+  this.setState({
+    movieList
+  });
+    
+  //TODO : console을 찍어서 확인해봅시다 
+  console.log(movieList);
+  };
+    
+
   render() {
     const { movieList } = this.state;
     return (
