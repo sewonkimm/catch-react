b7c2fd33024f88e62315b20857bb68452d8a1a0a
SeWonKim <swon962@gmail.com>
2020-02-27 13:04:24 +0900

    2호차 내용 정리 추가

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -5,7 +5,16 @@ export default class extends React.Component {
     movieDetail: []
   };
 
-  componentDidMount = async () => {};
+  componentDidMount = async () => {
+    //TODO: 1. 서버에서 데이터 불러오기
+
+    //TODO: 2. 브라우저에 임시로 저장하기 (state)
+    // this.setState({
+    //  ...
+    // })
+
+    
+  };
 
   render() {
     const { movieDetail } = this.state;
@@ -13,6 +22,7 @@ export default class extends React.Component {
       <div>
         <h3>영화 상세 페이지</h3>
         <div>상세한 영화에 대한 정보가 노출될 페이지입니다</div>
+        {/* TODO: 3. 영화 1개에 대한 데이터 출력하는 코드 짜기 */}
       </div>
     );
   }
