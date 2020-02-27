4c4f45563e813cde92e1cba5939e6d8ce60ff2b8
SeWonKim <swon962@gmail.com>
2020-02-27 13:21:12 +0900

    2회차 과제 1 - API를 사용하여 영화 5개 추가하기

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -10,7 +10,7 @@ export default class extends React.Component {
   componentDidMount = async () => {
 
   // 데이터를 불러올 API 주소
-  let url = "http://54.180.149.147:8080/api/movie";
+  let url = "http://54.180.149.147:8080/api/team/2/movie";
     
   //데이터를 서버에서 불러와서 movieList라는 변수에 저장한다 
   let movieList = await superagent
