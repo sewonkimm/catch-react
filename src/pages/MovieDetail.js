0442a0bcdd1c6fcf6bf5ccef09475cd60ecaff42
SeWonKim <swon962@gmail.com>
2020-02-28 20:12:50 +0900

    2주차 실습 리뷰

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -9,8 +9,12 @@ export default class extends React.Component {
   componentDidMount = async () => {
     //TODO: 1. 서버에서 데이터 불러오기
 
+    // URL에서 id 가져오기
+    const { match } = this.props;
+    let movieId = match.params.id;
+
     // 데이터를 불러올 API 주소
-    let url = "http://54.180.149.147:8080/api/team/2/movie/" + this.props.match.params.id;
+    let url = "http://54.180.149.147:8080/api/team/2/movie/" + movieId;
 
     //데이터를 서버에서 불러와서 movie라는 변수에 저장한다 
   let movieDetail = await superagent
@@ -28,20 +32,21 @@ export default class extends React.Component {
     const { movieDetail } = this.state;
 
     return (
-      <div>
-        <h3>영화 상세 페이지</h3>
-        <div>상세한 영화에 대한 정보가 노출될 페이지입니다</div>
-        {/* TODO: 3. 영화 1개에 대한 데이터 출력하는 코드 짜기 */
-          <MovieItem
-          title={movieDetail.title}
-          posterUrl={movieDetail.posterUrl}
-          advanceRate={movieDetail.advanceRate}
-          advanceRateRank={movieDetail.advanceRateRank}
-          />
-        }
-      </div>
-    );
-  }
+        <div>
+          <h3>영화 상세 페이지</h3>
+          <div>상세한 영화에 대한 정보가 노출될 페이지입니다</div>
+          {/* TODO: 3. 영화 1개에 대한 데이터 출력하는 코드 짜기 */
+            movieDetail ?
+              <MovieItem
+              title={movieDetail.title}
+              posterUrl={movieDetail.posterUrl}
+              advanceRate={movieDetail.advanceRate}
+              advanceRateRank={movieDetail.advanceRateRank}
+              />
+            : null}
+        </div>
+      );
+    }
 }
 
 const MovieItem = ({ title, posterUrl, advanceRate, advanceRateRank }) => {
