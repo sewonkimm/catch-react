2d68713a952cf5a0a7aa3e48f5b300b27f504a64
Siromom <swon962@gmail.com>
2020-03-02 14:11:01 +0900

    Movie Detail css

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -16,15 +16,15 @@ export default class extends React.Component {
     // 데이터를 불러올 API 주소
     let url = "http://54.180.149.147:8080/api/team/2/movie/" + movieId;
 
-    //데이터를 서버에서 불러와서 movie라는 변수에 저장한다 
-  let movieDetail = await superagent
-  .get(url)
-  .then(response => response.body)
-  .catch(error => error);
+    //데이터를 서버에서 불러와서 movie라는 변수에 저장한다
+    let movieDetail = await superagent
+      .get(url)
+      .then(response => response.body)
+      .catch(error => error);
 
     //TODO: 2. 브라우저에 임시로 저장하기 (state)
     this.setState({
-     movieDetail
+      movieDetail
     });
   };
 
@@ -32,29 +32,38 @@ export default class extends React.Component {
     const { movieDetail } = this.state;
 
     return (
-        <div>
-          <h3>영화 상세 페이지</h3>
-          {/* TODO: 3. 영화 1개에 대한 데이터 출력하는 코드 짜기 */
-            movieDetail ?
-              <MovieItem
-              title={movieDetail.title}
-              posterUrl={movieDetail.posterUrl}
-              advanceRate={movieDetail.advanceRate}
-              advanceRateRank={movieDetail.advanceRateRank}
-              />
-            : null}
-        </div>
-      );
-    }
+      <div className="movie_detail_screen">
+        {/* TODO: 3. 영화 1개에 대한 데이터 출력하는 코드 짜기 */
+        movieDetail ? (
+          <MovieItem
+            title={movieDetail.title}
+            posterUrl={movieDetail.posterUrl}
+            advanceRate={movieDetail.advanceRate}
+            advanceRateRank={movieDetail.advanceRateRank}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 const MovieItem = ({ title, posterUrl, advanceRate, advanceRateRank }) => {
   return (
-    <div>
-      <h3>{title}</h3>
-      <img width="150" src={posterUrl}/>
-      <div>예매율: {advanceRate}</div>
-      <div>예매율 순위 : {advanceRateRank}</div>
+    <div className="movie_item">
+      <div className="item_left">
+        <h3 className="movie_title">{title}</h3>
+
+        <div className="movie_advance_rate">
+          <div className="p_left">예매율</div>
+          <div className="p_right">{advanceRate}</div>
+        </div>
+        <div className="movie_advance_rate_rank">
+          <div className="p_left">예매율 순위</div>
+          <div className="p_right">{advanceRateRank}</div>
+        </div>
+      </div>
+
+      <img className="movie_poster" src={posterUrl} />
     </div>
   );
 };
