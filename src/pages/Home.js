1630b5802edfcd36e937de0763fea16535bb1246
SeWonKim <swon962@gmail.com>
2020-03-04 20:17:07 +0900

    change github img src

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
      <a href="/movie/detail/1">영화 상세 페이지 </a>
 
       <div className="github">
-        <img src="https://lh3.googleusercontent.com/proxy/aQ6CDb9JS5ipA1gOXdE6EopYQGkoPha3g_kfrbc6o85phGHY-E2vFcvKu_Hs_u8kK1121fPuTwECkwWlIClrvAuCJcqU_hHIn4njv1_D4qfV9_5mX0ToFHVBaE9gGw"/>
+        <img src="/images/github-icon.png"/>
         <a href="https://github.com/sewonkimm/catch-react">github page is here</a>
       </div>
     </div>
